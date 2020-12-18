094eb4ac75d0f44d2ecc0c5cbe9d902a1269e77a
Deepankar Sharma <59111795+deepankarcoder1994@users.noreply.github.com>
2020-12-19 01:30:27 +0530

    Add files via upload
    Committing all the files learnt in Maven Tutorials of automation Code

---
+++ RESTAPITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void posJira() {
+		System.out.println("posJira");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
